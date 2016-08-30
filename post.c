45281a892c1c89824704f9a9cbc58ab1511a1ff1
Rodrigo Costa <rcms@br.ibm.com>
2016-08-30 09:13:26 -0300

    Removing unnecessary code and calling free/unref functions.

--- post.c
+++ post.c
@@ -7,8 +7,8 @@
 
 static SoupSession *session;
 static GMainLoop *loop;
-static const gchar *output_file_path = NULL;
-static gboolean head, quiet;
+static const char *url = 
+    "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify";
 
 static void
 finished (SoupSession *session, SoupMessage *msg, gpointer loop)
@@ -17,19 +17,17 @@ finished (SoupSession *session, SoupMessage *msg, gpointer loop)
 }
 
 static void
-get_url (const char *api_key, const char *filepath)
+call_service (const char *api_key, const char *filepath)
 {
 	const char *name;
 	SoupMessage *msg;
   gsize length;
   guchar *buffer;
   gchar *uri;
-	FILE *output_file = NULL, *file = NULL;
+	FILE *file = NULL;
   SoupURI *soupURI = NULL;
   SoupMultipart *multipart = NULL;
   SoupBuffer *soup_buffer = NULL;
-  const char *url = 
-    "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify";
 
   file = fopen (filepath, "rb");
   if (!file)
@@ -44,7 +42,6 @@ get_url (const char *api_key, const char *filepath)
 
   buffer = g_malloc (length + 1); 
   fread (buffer, sizeof(guchar), length, file);
-
   fclose (file);
 
   soup_buffer = soup_buffer_new_take (buffer, length);
@@ -60,49 +57,27 @@ get_url (const char *api_key, const char *filepath)
 
   uri = soup_uri_to_string (soupURI, FALSE);
 	msg = soup_form_request_new_from_multipart (uri, multipart);
-  soup_uri_free (soupURI);
-  g_free (uri);
 
 	soup_message_set_flags (msg, SOUP_MESSAGE_NO_REDIRECT);
 
-	if (loop) {
-		g_object_ref (msg);
-		soup_session_queue_message (session, msg, finished, loop);
-		g_main_loop_run (loop);
-	} else
-		soup_session_send_message (session, msg);
+  g_object_ref (msg);
+  soup_session_queue_message (session, msg, finished, loop);
+  g_main_loop_run (loop);
+
+  soup_uri_free (soupURI);
+  soup_buffer_free (soup_buffer);
+  soup_multipart_free (multipart);
+  g_free (uri);
 
 	name = soup_message_get_uri (msg)->path;
 
-  if (msg->status_code == SOUP_STATUS_SSL_FAILED) {
-    GTlsCertificateFlags flags;
-
-    if (soup_message_get_https_status (msg, NULL, &flags))
-      g_print ("%s: %d %s (0x%x)\n", name, msg->status_code, msg->reason_phrase, flags);
-    else
-      g_print ("%s: %d %s (no handshake status)\n", name, msg->status_code, msg->reason_phrase);
-  } else if (!quiet || SOUP_STATUS_IS_TRANSPORT_ERROR (msg->status_code))
+  if (SOUP_STATUS_IS_TRANSPORT_ERROR (msg->status_code))
     g_print ("%s: %d %s\n", name, msg->status_code, msg->reason_phrase);
 
-	if (!head && SOUP_STATUS_IS_SUCCESSFUL (msg->status_code)) {
-		if (output_file_path) {
-			output_file = fopen (output_file_path, "w");
-			if (!output_file)
-				g_printerr ("Error trying to create file %s.\n", output_file_path);
-		} else if (!quiet)
-			output_file = stdout;
+	if (SOUP_STATUS_IS_SUCCESSFUL (msg->status_code)) 
+    fprintf (stdout, "%s", msg->response_body->data);
 
-		if (output_file) {
-			fwrite (msg->response_body->data,
-				1,
-				msg->response_body->length,
-				output_file);
-
-			if (output_file_path)
-				fclose (output_file);
-		}
-	}
-  soup_buffer_free (soup_buffer);
+  g_object_unref (msg);
 }
 
 int
@@ -129,7 +104,7 @@ main (int argc, char **argv)
 
   loop = g_main_loop_new (NULL, TRUE);
 
-  get_url (api_key, filepath);
+  call_service (api_key, filepath);
   g_main_loop_unref (loop);
   g_object_unref (session);
 
